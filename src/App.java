70b77f304a94e3b97171f8ad78b0415aaf0564fe
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2024-03-18 08:53:03 -0400

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        permutations(3, "");
+    }
+
+    static void permutations(int n, String current){
+   
+           
+        
+    }
+}
